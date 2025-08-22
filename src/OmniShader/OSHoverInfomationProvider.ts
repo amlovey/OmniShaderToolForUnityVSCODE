34deebb0a98f70f3fa23adb98f9edd1b514dd13e
Rocky <rockylai@medimension.com>
2025-08-22 08:44:09 +0800

    Update OSHoverInfomationProvider.ts

--- src/OmniShader/OSHoverInfomationProvider.ts
+++ src/OmniShader/OSHoverInfomationProvider.ts
@@ -6,8 +6,8 @@ export class OSHoverInformationProvider implements vscode.HoverProvider {
     provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover> {
         return fetchHover(document, position).then(info => {
             return new vscode.Hover([
-                new vscode.MarkdownString(info.doc),
-                { language: SHDAR_LANGUAGE_ID, value: info.value }
+                { language: SHDAR_LANGUAGE_ID, value: info.value },
+                new vscode.MarkdownString(info.doc)
             ]);
         });
     }
